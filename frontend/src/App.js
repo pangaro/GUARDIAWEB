925c98c1d7e30792ed927cc4328932710ec6fa50
Pangaro <fabpare@gmail.com>
2021-08-19 18:59:20 -0300

    v

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route
-        
-        />
-        
-      </Switch>
-    </Router>
+<h1>prueba react</h1>
   );
 }
 
---
+++ frontend/src/layout/Navbar.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav>
+            <li className="nav-item dropdown no-arrow">
+                <a className="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
+                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
+                    <img class="img-profile rounded-circle"
+                        src="img/undraw_profile.svg">
+                </a>
+                <div className="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                    aria-labelledby="userDropdown">
+                    <a class="dropdown-item" href="#">
+                        <i className="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                        Profile
+                    </a>
+                    <a className="dropdown-item" href="#">
+                        <i className="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
+                        Settings
+                    </a>
+                    <a className="dropdown-item" href="#">
+                        <i className="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
+                        Activity Log
+                    </a>
+                    <div className="dropdown-divider"></div>
+                    <a className="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                        <i className="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                        Logout
+                    </a>
+                </div>
+            </li>
+        </ul>
+    </nav>
+    )
+}
+
+export default Navbar
